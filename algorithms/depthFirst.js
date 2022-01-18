c864b470a552cb5db6a3265c1e8c552fc07d32ef
sajad <mss.sadra137812133@gmail.com>
2022-01-18 18:10:42 +0330

    Failed depthFirst

--- algorithms/depthFirst.js
+++ algorithms/depthFirst.js
@@ -2,59 +2,39 @@ const tools = require("./tools"),
     home = require("../config/home");
 
 let MIN = 999999,
-    optimalPath = [];
+    optimalPath = [],
+    TABLE = [];
 
 module.exports = (table, sx, sy, ex, ey) => {
-
-    // First initialization
-    let visited = []
-    for (let i = 0; i < 20; i++) {
-        let temp = [];
-        for (let j = 0; j < 20; j++)
-            temp.push(false);
-        visited.push(temp)
-    }
-
-    algorithm(table, sx, sy, ex, ey, visited, []);
-
-    console.log(optimalPath);
-
-    optimalPath.forEach((node_id) => {
+    TABLE = tools.clone_2d_array(table);
+    let path = algorithm(sx, sy, ex, ey, []);
+    path.forEach((node_id) => {
         let xy = tools.get_xy(node_id);
         table[xy[0]][xy[1]] = home.type.tracked;
     });
     return table;
 }
 
-function algorithm(table, sx, sy, ex, ey, visited, path) {
+function algorithm(sx, sy, ex, ey, path) {
 
-    // If Indexes are Out of Bound then Return from Branch
-    if (sx < 0 || sy < 0 || sx >= 20 || sy >= 20) return;
+    if (sx < 0 || sx >= 20) return null;
+    if (sy < 0 || sy >= 20) return null;
 
+    if (sx === ex && sy === ey) return path;
+    if (TABLE[sx][sy] === home.type.wall) return null;
+
+    TABLE[sx][sy] = home.type.wall;
     path.push(tools.get_id(sx, sy));
 
-    // Goal State Test
-    if (sx == ex && sy == ey) {
-        if (path.length <= MIN) {
-            MIN = path.length;
-            optimalPath = tools.clone_array(path);
-            return;
-        }
-    }
+    let res = (algorithm(sx + 1, sy, ex, ey, path)
+        || algorithm(sx, sy + 1, ex, ey, path)
+        || algorithm(sx - 1, sy, ex, ey, path)
+        || algorithm(sx, sy - 1, ex, ey, path));
 
-    // Horizontal and Vertical Search
-    if (sx - 1 >= 0 && !visited[sx - 1][sy] && table[sx - 1][sy] != home.type.wall) { // Up 
-        visited[sx - 1][sy] = true;
-        algorithm(table, sx - 1, sy, ex, ey, tools.clone_array(visited), tools.clone_array(path));
-    } if (sx + 1 < 20 && !visited[sx + 1][sy] && table[sx + 1][sy] != home.type.wall) { // Down
-        visited[sx + 1][sy] = true;
-        algorithm(table, sx + 1, sy, ex, ey, tools.clone_array(visited), tools.clone_array(path));
-    } if (sy - 1 >= 0 && !visited[sx][sy - 1] && table[sx][sy - 1] != home.type.wall) { // Left
-        visited[sx][sy - 1] = true;
-        algorithm(table, sx, sy - 1, ex, ey, tools.clone_array(visited), tools.clone_array(path));
-    } if (sy + 1 < 20 && !visited[sx][sy + 1] && table[sx][sy + 1] != home.type.wall) { // Right
-        visited[sx][sy + 1] = true;
-        algorithm(table, sx, sy + 1, ex, ey, tools.clone_array(visited), tools.clone_array(path));
-    }
+    if (res != null) return res;
 
+    TABLE[sx][sy] = home.type.open;
+    path.pop();
+
+    return null;
 }
